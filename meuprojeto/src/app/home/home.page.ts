d1ce57be645ede4da9e52d582ed7e1bf2172b9f6
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-04-24 10:58:51 -0300

    Commit final do primeiro exemplo do material

--- meuprojeto/src/app/home/home.page.ts
+++ meuprojeto/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,84 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public actionSheetController: ActionSheetController,
+              public alertController: AlertController) { }
 
+  async apresentarActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Opções',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        id: 'delete-button',
+        data: {
+          type: 'delete'
+        },
+        handler: () => {
+          console.log('O botão delete foi clicado.');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share',
+        data: 10,
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'caret-forward-circle',
+        data: 'Data value',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+    const { role, data } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role and data ->', role, data);
+
+  }
+
+
+  async apresentarAlert() {
+    const alert = await this.alertController.create({
+    cssClass: 'my-custom-class',
+    header: 'Alert',
+    subHeader: 'Subtitle',
+    message: 'This is an alert message.',
+    buttons: [
+    {
+    text: 'Cancel',
+    role: 'cancel',
+    cssClass: 'secondary',
+    handler: () => {
+    console.log('Confirm Cancel');
+    }
+    }    , {
+    text: 'Ok',
+    role: 'ok',
+    handler: () => {
+      console.log('Confirm Ok');
+      }
+      }]
+    });
+    await alert.present();
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
 }
